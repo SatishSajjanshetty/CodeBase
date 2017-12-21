8e6a340673bd7e1a64404adc0b009b60636d9e84
Satish Sajjanshetty <ssajjanshetty@nthrive.com>
2017-12-20 23:34:48 -0500

    Compile Time Polymorphism Sample

---
+++ CSharpConcepts/Polymorphism_CompileTime/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Polymorphism_CompileTime
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var overload = new Overload();
+
+            //Based on parameter type and number of parameters respective overload method will be called
+            overload.DisplayOverload(100);
+            overload.DisplayOverload(100, 200);
+            overload.DisplayOverload(100, "Satish");
+      
+            Console.ReadKey();
+        }
+
+        public class Overload
+        {
+            //Return type is not a part of method signature
+           public int DisplayOverload(int a)
+            {
+                Console.WriteLine(a);
+                return a;
+            }
+
+            //Number of parameters are part of method signature
+            public int DisplayOverload(int a, int b)
+            {
+                Console.WriteLine(a + " " + b);
+                return a;
+            }
+
+            //Diffrent data type in parameters are part of method signature
+            public void DisplayOverload(int a, string b)
+            {
+                Console.WriteLine(a +" "+ b);
+            }
+        }
+    }
+
+
+
+
+}
